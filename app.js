153114179e7e488280e397272f1cf187195c18dd
Ojo Ayodeji <degzie24@gmail.com>
2023-09-11 11:16:04 +0100

    minor fixes

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const personRouter = require('./routes/personRouter');
 
 const app = express();
+
+// Set security HTTP headers
+app.use(helmet());
+
 app.use(express.json());
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
 
 app.use('/api', personRouter);
 
